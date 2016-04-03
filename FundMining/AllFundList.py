2668d731294cc687a3bbba8c5120956fc84a7acc
micerin <micerin@hotmail.com>
2016-04-03 20:29:16 +0800

    Add patterns to select out funds

--- FundMining/AllFundList.py
+++ FundMining/AllFundList.py
@@ -29,6 +29,7 @@ class Fund:
         self.thisyear = ''
         self.fromstartup = ''
         self.starttime = ''
+        self.weighted = ''
 
 #Get fund list per filter
 def getFundList(filter):
@@ -63,24 +64,6 @@ def parseFundList(content):
             fund.starttime = isplits[16]
             fundInfoList.append(fund)
 
-            '''
-            fundcodelist.append(isplits[0][1:])
-            namelist.append(isplits[1])
-            latestvalue.append(isplits[4])
-            overallvalue.append(isplits[5])
-            latestdelta.append(isplits[6])
-            weekdelta.append(isplits[7])
-            monthdelta.append(isplits[8])
-            threemonthdelta.append(isplits[9])
-            sixmonthdelta.append(isplits[10])
-            yeardelta.append(isplits[11])
-            twoyeardelta.append(isplits[12])
-            threeyeahdelta.append(isplits[13])
-            thisyeardelta.append(isplits[14])
-            fromstartup.append(isplits[15])
-            starttime.append(isplits[16])
-            '''
-
     #get overall data for all funds, all kinds
     #re_strforall = r'allNum:(.*),gpNum:(.*),hhNum:(.*),zqNum:(.*),zsNum:(.*),bbNum:(.*),qdiiNum:(.*),etfNum:(.*),lofNum:(.*)'
     #re_patforall = re.compile(re_strforall)
@@ -97,7 +80,6 @@ def saveAsCsv(flist, file):
         isplits = string.split(item, ',')
         if(len(isplits) > 0): # fund code
             isplits[0] = isplits[0][1:]
-            #fundcodelist.append(isplits[0])
         for sitem in isplits:
             if sitem !='"': #sitem != '' and
                 result.append(sitem)
@@ -106,8 +88,17 @@ def saveAsCsv(flist, file):
         result = []
     csvfiletowrite.close()
 
+#Check fund rank >=4
+def isHighlyRanked(foudcode):
+    rankUrlTemp = 'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=grade&code=%s&page=1&per=1'
+    rankUrl = rankUrlTemp % foudcode
+    content = urllib2.urlopen(rankUrl).read()
+    if '\'4\'' in content or '\'5\'' in content:
+        return True
+    return False
+
 #Parameters
-typeFilter = 'hh' # types allNum:2602,gpNum:469,hhNum:1174,zqNum:734,zsNum:344,bbNum:100,qdiiNum:94,etfNum:0,lofNum:147
+typeFilter = 'lof' # types allNum:2602,gpNum:469,hhNum:1174,zqNum:734,zsNum:344,bbNum:100,qdiiNum:94,etfNum:0,lofNum:147
 sTime = '2015-04-03'
 
 now = int(time.time())
@@ -117,35 +108,78 @@ eTime = time.strftime("%Y-%m-%d", timeArray)#'2016-04-03'
 num = 10000 #Max number fund to load(10000 for all funds)
 filecsv = 'funds.csv'
 filters = (typeFilter, sTime, eTime, num)
-'''
-fundcodelist = [] #store fund code
-namelist = []
-latestvalue = []
-overallvalue = []
-latestdelta = []
-weekdelta = []
-monthdelta = []
-threemonthdelta = []
-sixmonthdelta = []
-yeardelta = []
-twoyeardelta = []
-threeyeahdelta = []
-thisyeardelta = []
-fromstartup = []
-starttime = []
-'''
-
 fundInfoList = []
 
 #Main calls
 listcontent = getFundList(filters)
 parseFundList(listcontent)
+topNum = 30
 
 #analyze and filter
-print 'Fund list 1:'
+
+print 'Fund list 1: 净值最高'
+def passed1(fund):
+    try:
+        string.atof(fund.latestvalue)
+        return True
+    except:
+        return False
+fundInfoList1 = filter(passed1, fundInfoList)
+fundInfoListOrdered1 = sorted(fundInfoList1,key=lambda fund:string.atof(fund.latestvalue), reverse=True)
+for i in range(0,topNum - 1):
+    fundlinkTemp = 'http://fund.eastmoney.com/%s.html'
+    fundlink = fundlinkTemp % fundInfoListOrdered1[i].fundcode
+    print '   %s %s %.3f' % (fundlink , fundInfoListOrdered1[i].name, string.atof(fundInfoListOrdered1[i].latestvalue))
+
+print 'Fund list 2: 最近3个月增长最快，并且成立两年以上'
+def passed2(fund):
+    try:
+        string.atof(fund.threemonthdelta)
+        string.atof(fund.twoyeardelta)
+        return True
+    except:
+        return False
+fundInfoList2 = filter(passed2, fundInfoList)
+fundInfoListOrdered2 = sorted(fundInfoList2, key=lambda fund:string.atof(fund.threemonthdelta),reverse=True)
+meetnum = 0
+for i in range(0, len(fundInfoListOrdered2)):
+    fundlinkTemp = 'http://fund.eastmoney.com/%s.html'
+    fundlink = fundlinkTemp % fundInfoListOrdered2[i].fundcode
+    print '   %s %s 净值：%s 近三个月：%.3f 两年数据：%s' % (fundlink , fundInfoListOrdered2[i].name, fundInfoListOrdered2[i].latestvalue, string.atof(fundInfoListOrdered2[i].threemonthdelta), fundInfoListOrdered2[i].twoyeardelta)
+    meetnum +=1
+    if meetnum == topNum:
+        break
 
 
+print 'Fund list 3: 权重 0.65*近半年 + 0.3*近一年 + 0.05*近两年, 且评级大于等于4星'
+weight= 0
+def passed3(fund):
+    try:
+        string.atof(fund.halfyeardelta)
+        string.atof(fund.yeardelta)
+        string.atof(fund.twoyeardelta)
+        return True
+    except:
+        return False
 
+fundInfoList3 = filter(passed3, fundInfoList)
+for item in fundInfoList3:
+    item.weighted = string.atof(item.halfyeardelta)* 0.65 + string.atof(item.yeardelta)*0.3 + string.atof(item.twoyeardelta)*0.05
+    #for zq
+    #item.weighted = string.atof(item.halfyeardelta)* 0.3 + string.atof(item.yeardelta)*0.4 + string.atof(item.twoyeardelta)*0.3
 
-print 'completed'
+fundInfoListOrdered3 = sorted(fundInfoList3, key=lambda fund:string.atof(fund.weighted),reverse=True)
+
+meetnum = 0
+for i in range(0, len(fundInfoListOrdered3)):
+    fundlinkTemp = 'http://fund.eastmoney.com/%s.html'
+    #print '%s %s 净值：%s 权重：%s' % (fundInfoListOrdered3[i].fundcode , fundInfoListOrdered3[i].name, fundInfoListOrdered3[i].latestvalue, fundInfoListOrdered3[i].weighted)
+    if isHighlyRanked(fundInfoListOrdered3[i].fundcode):
+        fundlink = fundlinkTemp % fundInfoListOrdered3[i].fundcode
+        print '   %s %s 净值：%s 权重：%s' % (fundlink , fundInfoListOrdered3[i].name, fundInfoListOrdered3[i].latestvalue, fundInfoListOrdered3[i].weighted)
+        meetnum +=1
+    if meetnum == topNum:
+        break
+
+print 'Completed'
 
